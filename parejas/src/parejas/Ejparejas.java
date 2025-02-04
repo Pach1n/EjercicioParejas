3ebb12f6edb28f95fdd27d19ac7702f38bf1cda7
marcjack18 <marcospp172006@gmail.com>
2025-02-04 09:50:38 +0100

    kk
    Signed-off-by: marcjack18 <marcospp172006@gmail.com>

--- parejas/src/parejas/Ejparejas.java
+++ parejas/src/parejas/Ejparejas.java
@@ -6,109 +6,112 @@ class Palabra {
 
 	String termino;
 	String definicion;
+
 	
 	
-	
-	
-	Palabra(String termino, String definicion) {
+	public  Palabra(String termino, String definicion) {
 		this.termino = termino;
 		this.definicion = definicion;
+	}
+	
+	public Palabra() {
+		this.termino="";
+		this.definicion="";
 	}
 
 	public String getTermino() {
 		return termino;
 	}
-	
+
 	public void setTermino(String termino) {
 		this.termino = termino;
 	}
-	
+
 	public String getDefinicion() {
 		return definicion;
 	}
-	
+
 	public void setDefinicion(String definicion) {
 		this.definicion = definicion;
 	}
 
 }
 
-
-
-
-
-
-
-
 public class Ejparejas {
-	
-	
+
 	public static void mostrarMenu() {
-		
+
 		System.out.println("1) Introducir un término y su definición");
 		System.out.println("2) Mostar todos los términos (sin definiciones)");
 		System.out.println("3) Obtener la definición de un término que el usuario introduzca");
 		System.out.println("4) Eliminar un término y su definición");
 		System.out.println("5) Salir");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int opc;
+		String termino="";
 		int contadorPal = 0;
 		Palabra[] diccionario = new Palabra[100];
-		
+		Palabra pa=new Palabra();
+		int posicion=0;
 		do {
-			
+
 			Ejparejas.mostrarMenu();
 			opc = sc.nextInt();
 			
-			switch(opc) {
-			
+			switch (opc) {
+
 			case 1:
-				
+
 				System.out.println("Introduce un término");
 				String term = sc.next();
 				sc.nextLine();
 				System.out.println("Introduce una definición para ese término");
 				String def = sc.nextLine();
-				diccionario[contadorPal] = new Palabra(term, def);
+				pa=new Palabra(term,def);
+				diccionario[contadorPal] = pa;
 				contadorPal++;
 				break;
-				
+
 			case 2:
-				for(int i = 0; i < contadorPal; i++) {
+				for (int i = 0; i < contadorPal; i++) {
 					System.out.print(diccionario[i].getTermino());
 					System.out.println();
 				}
+				break;
+				
+			case 3:
+				System.out.println("Dime un termino");
+				termino=sc.next();
+				sc.nextLine();
+				for(int i=0;i<contadorPal;i++) {
+				if(diccionario[i].getTermino().equals(termino)) {
+					System.out.println("La definicion es: " + pa.getDefinicion());
+				}
+				}
+	
+				break;
+			case 4:
+				System.out.println("Dime un termino");
+				termino=sc.next();
+				sc.nextLine();
 				
 				
-				
-			
-			
+				for(int i=0;i<contadorPal;i++) {
+					if(diccionario[i].getTermino().equals(termino)) {
+						posicion=i;
+					}
+				}
+				for(int j=posicion+1;j<contadorPal;j++) {
+				diccionario[j-1]=diccionario[j];
+				}
+				contadorPal--;
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 		} while (opc != 5);
-		
-		
-		
-		
 
 	}
 
-} 
+}
