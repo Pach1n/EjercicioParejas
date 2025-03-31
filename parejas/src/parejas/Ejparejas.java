f5c9be5afb07d3fe8cbdf9ee28294a7c7983166e
Pach1n <ivanx.afonso@gmail.com>
2025-03-31 12:58:58 +0200

    Update Ejparejas.java

--- parejas/src/parejas/Ejparejas.java
+++ parejas/src/parejas/Ejparejas.java
@@ -1,117 +1,41 @@
-package parejas;
+package com.hibernate.util;
 
-import java.util.Scanner;
+import java.util.Properties;
 
-class Palabra {
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
-	String termino;
-	String definicion;
+import com.hibernate.model.Persona;
 
-	
-	
-	public  Palabra(String termino, String definicion) {
-		this.termino = termino;
-		this.definicion = definicion;
-	}
-	
-	public Palabra() {
-		this.termino="";
-		this.definicion="";
-	}
+public class Config {
 
-	public String getTermino() {
-		return termino;
-	}
-
-	public void setTermino(String termino) {
-		this.termino = termino;
-	}
-
-	public String getDefinicion() {
-		return definicion;
-	}
-
-	public void setDefinicion(String definicion) {
-		this.definicion = definicion;
-	}
-
-}
-
-public class Ejparejas {
-
-	public static void mostrarMenu() {
-
-		System.out.println("1) Introducir un término y su definición");
-		System.out.println("2) Mostar todos los términos (sin definiciones)");
-		System.out.println("3) Obtener la definición de un término que el usuario introduzca");
-		System.out.println("4) Eliminar un término y su definición");
-		System.out.println("5) Salir");
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int opc;
-		String termino="";
-		int contadorPal = 0;
-		Palabra[] diccionario = new Palabra[100];
-		Palabra pa=new Palabra();
-		int posicion=0;
-		do {
-
-			Ejparejas.mostrarMenu();
-			opc = sc.nextInt();
-			
-			switch (opc) {
-
-			case 1:
-
-				System.out.println("Introduce un término");
-				String term = sc.next();
-				sc.nextLine();
-				System.out.println("Introduce una definición para ese término");
-				String def = sc.nextLine();
-				pa=new Palabra(term,def);
-				diccionario[contadorPal] = pa;
-				contadorPal++;
-				break;
-
-			case 2:
-				for (int i = 0; i < contadorPal; i++) {
-					System.out.print(diccionario[i].getTermino());
-					System.out.println();
-				}
-				break;
+	private static SessionFactory sessionFactory;
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
 				
-			case 3:
-				System.out.println("Dime un termino");
-				termino=sc.next();
-				sc.nextLine();
-				for(int i=0;i<contadorPal;i++) {
-				if(diccionario[i].getTermino().equals(termino)) {
-					System.out.println("La definicion es: " + pa.getDefinicion());
-				}
-				}
-	
-				break;
-			case 4:
-				System.out.println("Dime un termino");
-				termino=sc.next();
-				sc.nextLine();
+				Configuration configuration = new Configuration();
 				
-				
-				for(int i=0;i<contadorPal;i++) {
-					if(diccionario[i].getTermino().equals(termino)) {
-						posicion=i;
-					}
-				}
-				for(int j=posicion+1;j<contadorPal;j++) {
-				diccionario[j-1]=diccionario[j];
-				}
-				contadorPal--;
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3307/agenda?useSSL=false");
+				settings.put(Environment.USER, "alumno");
+				settings.put(Environment.PASS, "Alumno1219");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(Persona.class);
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-		} while (opc != 5);
-
+		}
+		return  sessionFactory;
 	}
-
+	
+	
 }
